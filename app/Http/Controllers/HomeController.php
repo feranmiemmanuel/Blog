91adb35f357193c14733496707fecdbb3f7514fd
feranmiemmanuel <feranmiolasunkanmi91@gmail.com>
2022-03-08 13:50:42 +0100

    category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
   public function home(){
     $posts = Post::all();
-    return view('home',['posts'=>$posts]);
+    $categories = Category::all();
+    return view('home',['posts'=>$posts, 'categories'=>$categories]);
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,32 +113,19 @@
           <h5 class="card-header">Categories</h5>
           <div class="card-body">
             <div class="row">
+            @if(count($categories)>0)
+              @foreach($categories as $category)
               <div class="col-lg-6">
                 <ul class="list-unstyled mb-0">
                   <li>
-                    <a href="#">Web Design</a>
-                  </li>
-                  <li>
-                    <a href="#">HTML</a>
-                  </li>
-                  <li>
-                    <a href="#">Freebies</a>
+                    <a href="#">{{$category->title}}</a>
                   </li>
                 </ul>
               </div>
-              <div class="col-lg-6">
-                <ul class="list-unstyled mb-0">
-                  <li>
-                    <a href="#">JavaScript</a>
-                  </li>
-                  <li>
-                    <a href="#">CSS</a>
-                  </li>
-                  <li>
-                    <a href="#">Tutorials</a>
-                  </li>
-                </ul>
-              </div>
+              @endforeach
+              @else
+              <p class="alert alert-danger">No Category Found</p>
+              @endif
             </div>
           </div>
         </div>
