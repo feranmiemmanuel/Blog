4bec6d939fbf154d44a8a339d6b9b585f6889a86
feranmiemmanuel <feranmiolasunkanmi91@gmail.com>
2022-03-14 10:58:03 +0100

    pagination

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,15 +80,17 @@
           @endif
         </div>
 
+        {{$posts->links()}}
+
         <!-- Pagination -->
-        <ul class="pagination justify-content-center mb-4">
+        <!--<ul class="pagination justify-content-center mb-4">
           <li class="page-item">
             <a class="page-link" href="#">&larr; Older</a>
           </li>
           <li class="page-item disabled">
             <a class="page-link" href="#">Newer &rarr;</a>
           </li>
-        </ul>
+        </ul>-->
 
       </div>
 
