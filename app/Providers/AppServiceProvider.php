a735c99c9272d89d6225b0e4bb23cfa211c3be48
feranmiemmanuel <feranmiolasunkanmi91@gmail.com>
2022-03-14 15:16:59 +0100

    recent_posts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        View::share('recent_posts',\App\Models\Post::orderBy('id', 'desc')->limit('5')->get());
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -140,28 +140,13 @@
             <div class="row">
               <div class="col-lg-6">
                 <ul class="list-unstyled mb-0">
-                  <li>
-                    <a href="#">Web Design</a>
+                @if($recent_posts)
+                  @foreach($recent_posts as $post)
+                    <li>
+                    <a href="#">{{$post->title}}</a>
                   </li>
-                  <li>
-                    <a href="#">HTML</a>
-                  </li>
-                  <li>
-                    <a href="#">Freebies</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-lg-6">
-                <ul class="list-unstyled mb-0">
-                  <li>
-                    <a href="#">JavaScript</a>
-                  </li>
-                  <li>
-                    <a href="#">CSS</a>
-                  </li>
-                  <li>
-                    <a href="#">Tutorials</a>
-                  </li>
+                  @endforeach
+                @endif
                 </ul>
               </div>
             </div>
